5185f30f1e5f344aaed9e21b8fc8891473b12bc3
gresa <gresalika6@gmail.com>
2024-08-10 13:53:05 +0200

    Inital
    Initial

---
+++ FoodieDeliveriesWeb/FoodieDeliveriesWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodieDeliveriesWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
